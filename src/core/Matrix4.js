2514950945a654f09dd316d0f98d106562ad4c99
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-05-27 16:10:08 +0200

    Updated example

--- src/core/Matrix4.js
+++ src/core/Matrix4.js
@@ -25,7 +25,7 @@ GLOW.Matrix4 = function() {
 	that.update = function() {
 		
 		that.setRotationFromEuler( rotation );
-		
+		return that;
 	}
 
 	that.set = function( m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44 ) {
