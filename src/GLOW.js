432424d593b49e311a797430fbeca026f22344fd
Olov Lassus <olov.lassus@gmail.com>
2011-06-01 23:25:10 +0200

    GLOW.js: deflufflify, untabify, semicolons

--- src/GLOW.js
+++ src/GLOW.js
@@ -1,75 +1,49 @@
 /*
-* GLOW - Just-Above-Low-Level WebGL API
+* Glow - Just-Above-Low-Level WebGL API
 */
 
 var GLOW = (function() {
-	
-	var glow = {}; 
-	var contexts = {};
-	var uniqueIdCounter = -1;
+    var glow = {};
+    var contexts = {};
+    var uniqueIdCounter = -1;
 
-	glow.currentContext = {};
+    glow.currentContext = {};
 
+    //--- register context ---
+    glow.registerContext = function( context ) {
+        contexts[ context.id ] = context;
+        glow.enableContext( context );
+    };
 
-	//--- register context ---
+    //--- get context by id ---
+    glow.getContextById = function( id ) {
+        if( contexts[ id ] ) {
+            return contexts[ id ];
+        }
+        console.error( "Couldn't find context id " + id + ", returning current with id " + glow.currentContext.id );
+        return glow.currentContext;
+    };
 
-	glow.registerContext = function( context ) {
-		
-		contexts[ context.id ] = context;
-		glow.enableContext( context );
-	};
-	
-	
-	//--- get context by id ---
-	
-	glow.getContextById = function( id ) {
-		
-		if( contexts[ id ] ) {
-			
-			return contexts[ id ];
-		}
-			
-		console.error( "Couldn't find context id " + id + ", returning current with id " + glow.currentContext.id );
-		return glow.currentContext;
-	};
+    //--- enable context ---
+    glow.enableContext = function( contextOrId ) {
+        if( typeof( contextOrId ) === 'string' ) {
+            glow.currentContext = getContextById(contextOrId);
+        } else {
+            glow.currentContext = contextOrId;
+        }
+        GL = glow.GL = glow.currentContext.GL;
+    };
 
+    //--- unique id ---
+    glow.uniqueId = function() {
+        return ++uniqueIdCounter;
+    };
 
-	//--- enable context ---
-
-	glow.enableContext = function( contextOrId ) {
-		
-		if( typeof( contextOrId ) === 'string' ) {
-			
-			glow.currentContext = getContextById(contextOrId);
-			
-		} else {
-			
-			glow.currentContext = contextOrId;
-		}
-		
-		GL = glow.GL = glow.currentContext.GL;
-	
-	}
-
-
-	//--- unique id ---
-	
-	glow.uniqueId = function() {
-		
-		return ++uniqueIdCounter;
-	}
-
-
-	//--- return public ---
-
-	return glow;
-	
+    //--- return public ---
+    return glow;
 }());
-
-
 
 /*
 * Current GL - set to latest registered or enabled GL context 
 */
-
 var GL = {};
