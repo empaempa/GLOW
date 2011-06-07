da0468c0857a45c4313961ab4c4ecb7992dbe8b2
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-06-07 16:37:43 +0200

    Added set,add,sub... methods

--- src/extras/math/Float.js
+++ src/extras/math/Float.js
@@ -3,10 +3,46 @@
 * @author: Mikael Emtinger, gomo.se
 */
 
-GLOW.Float = function( value ) {
+GLOW.Float = (function() {
 	
 	"use strict";
 	
-	this.value = new Float32Array( 1 );
-	this.value[ 0 ] = value !== undefined ? value : 0;
-}
+    // constructor
+	function float( value ) {
+    	this.value = new Float32Array( 1 );
+    	this.value[ 0 ] = value !== undefined ? value : 0;
+	}
+
+    // methods
+    float.prototype.set = function( value ) {
+        this.value[ 0 ] = value;
+        return this;
+    }
+
+    float.prototype.add = function( value ) {
+        this.value[ 0 ] += value;
+        return this;
+    }
+
+    float.prototype.sub = function( value ) {
+        this.value[ 0 ] -= value;
+        return this;
+    }
+
+    float.prototype.multiply = function( value ) {
+        this.value[ 0 ] *= value;
+        return this;
+    }
+
+    float.prototype.divide = function( value ) {
+        this.value[ 0 ] /= value;
+        return this;
+    }
+
+    float.prototype.modulo = function( value ) {
+        this.value[ 0 ] %= value;
+        return this;
+    }
+	
+	return float;
+})();
--- src/extras/math/Int.js
+++ src/extras/math/Int.js
@@ -6,6 +6,43 @@
 GLOW.Int = function( value ) {
 
 	"use strict";
-	this.value = new Float32Array( 1 );
-	this.value[ 0 ] = value !== undefined ? value : 0;
+	
+    // constructor
+	function int( value ) {
+    	this.value = new int32Array( 1 );
+    	this.value[ 0 ] = value !== undefined ? value : 0;
+	}
+
+    // methods
+    int.prototype.set = function( value ) {
+        this.value[ 0 ] = value;
+        return this;
+    }
+
+    int.prototype.add = function( value ) {
+        this.value[ 0 ] += value;
+        return this;
+    }
+
+    int.prototype.sub = function( value ) {
+        this.value[ 0 ] -= value;
+        return this;
+    }
+
+    int.prototype.multiply = function( value ) {
+        this.value[ 0 ] *= value;
+        return this;
+    }
+
+    int.prototype.divide = function( value ) {
+        this.value[ 0 ] /= value;
+        return this;
+    }
+
+    int.prototype.modulo = function( value ) {
+        this.value[ 0 ] %= value;
+        return this;
+    }
+	
+	return int;
 }
