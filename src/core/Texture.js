d6190006a5e1630ad356067d2e249d847e92f770
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-07-16 14:28:19 +0200

    Added support for canvas and texture parameters

--- src/core/Texture.js
+++ src/core/Texture.js
@@ -3,33 +3,74 @@ GLOW.Texture = (function() {
 	"use strict"; "use restrict";
 	
 	// constructor
-	function texture( url ) {
-    	this.url = url;
+	function texture( parameters ) {
+    	if( parameters.url ) {
+        	this.url = parameters.url;
+        	var lowerCase = this.url.toLowerCase();
+        	if( lowerCase.indexOf( ".jpg" ) !== undefined || 
+                lowerCase.indexOf( ".png" ) !== undefined ||
+                lowerCase.indexOf( ".jpeg" ) !== undefined ) {
+                this.image = new Image();
+                this.image.scope = this;    
+            } else {
+                this.video = document.createElement( "video" );
+                this.video.scope = this;
+            }
+    	} else if( parameters.image ) {
+    	    this.image = parameters.image;
+    	} else if( parameters.canvas ) {
+    	    this.canvas = parameters.canvas;
+    	} else if( parameters.video ) {
+    	    this.video = parameters.video;
+    	}
+    	
     	this.id = GLOW.uniqueId();
+    	this.wrapS = parameters.wrapS !== undefined ? parameters.wrapS : parameters.wrap !== undefined ? parameters.wrap : GL.REPEAT;
+    	this.wrapT = parameters.wrapT !== undefined ? parameters.wrapT : parameters.wrap !== undefined ? parameters.wrap : GL.REPEAT;
+    	this.magFilter = parameters.magFilter !== undefined ? parameters.magFilter : GL.LINEAR;
+    	this.minFilter = parameters.minFilter !== undefined ? parameters.minFilter : GL.LINEAR_MIPMAP_LINEAR;
+    	this.mipmap = parameters.mipmap !== undefined ? parameters.mipmap : true;
     	this.textureUnit = -1;
     	this.texture = undefined;
-    	this.image = new Image();
-    	this.image.scope = this;
 	}
 
 	// methods
     texture.prototype.init = function( textureUnit ) {
     	this.textureUnit = textureUnit;
-    	this.image.onload = this.onLoad;
-    	this.image.src = this.url;
-    }
+    	if( this.image ) {
+        	this.image.onload = this.onLoad;
+        	this.image.src = this.url;
+    	} else if( this.video ) {
+    	    // TODO
+    	} else if( this.canvas ) {
+    	    this.createTexture();
+    	}
+    };
+    
+    texture.prototype.createTexture = function() {
+    	this.texture = GL.createTexture();
+    	GL.bindTexture( GL.TEXTURE_2D, this.texture );
+
+    	if( this.image ) {
+        	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this.image );
+    	} else if( this.video ) {
+        	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this.video );
+    	} else if( this.canvas ) {
+        	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this.canvas );
+    	}
+
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, this.wrapS );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, this.wrapT );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, this.minFilter );
+        if( this.mipmap ) {
+        	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, this.magFilter );
+        	GL.generateMipmap( GL.TEXTURE_2D );
+        }
+    };
 
     texture.prototype.onLoad = function() {
-    	var scope = this.scope;
-    	scope.texture = GL.createTexture();
-    	GL.bindTexture( GL.TEXTURE_2D, scope.texture );
-    	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this );
-    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT );
-    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT );
-    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR );
-    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR_MIPMAP_LINEAR );
-    	GL.generateMipmap( GL.TEXTURE_2D );
-    }
+    	this.scope.createTexture();
+    };
 
 	return texture;
 })();
