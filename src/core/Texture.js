031fcae9bfe9d9d31bb86e565110dcf6c04fbf24
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-07-16 14:01:51 +0200

    Refactored GLOW.Texture to use "restrict"

--- src/core/Texture.js
+++ src/core/Texture.js
@@ -1,43 +1,35 @@
-GLOW.Texture = function( url ) {
+GLOW.Texture = (function() {
 	
-	"use strict";
+	"use strict"; "use restrict";
 	
-	// TODO: add wrapping, min- mag filter options
-	
-	this.url = url;
-	this.id = GLOW.uniqueId();
-	this.textureUnit = -1;
-	this.texture = undefined;
-	this.image = new Image();
-	this.image.scope = this;
-}
+	// constructor
+	function texture( url ) {
+    	this.url = url;
+    	this.id = GLOW.uniqueId();
+    	this.textureUnit = -1;
+    	this.texture = undefined;
+    	this.image = new Image();
+    	this.image.scope = this;
+	}
 
-/* 
-* Prototype
-*/ 
+	// methods
+    texture.prototype.init = function( textureUnit ) {
+    	this.textureUnit = textureUnit;
+    	this.image.onload = this.onLoad;
+    	this.image.src = this.url;
+    }
 
-GLOW.Texture.prototype.init = function( textureUnit ) {
-	
-	this.textureUnit = textureUnit;
-	this.image.onload = this.onLoad;
-	this.image.src = this.url;
-	
-}
+    texture.prototype.onLoad = function() {
+    	var scope = this.scope;
+    	scope.texture = GL.createTexture();
+    	GL.bindTexture( GL.TEXTURE_2D, scope.texture );
+    	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR );
+    	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR_MIPMAP_LINEAR );
+    	GL.generateMipmap( GL.TEXTURE_2D );
+    }
 
-GLOW.Texture.prototype.onLoad = function() {
-
-	var scope = this.scope;
-	
-	scope.texture = GL.createTexture();
-	
-	GL.bindTexture( GL.TEXTURE_2D, scope.texture );
-	GL.texImage2D( GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, this );
-
-	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT );
-	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT );
-
-	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR );
-	GL.texParameteri( GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR_MIPMAP_LINEAR );
-
-	GL.generateMipmap( GL.TEXTURE_2D );
-}
+	return texture;
+})();
