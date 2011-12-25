52d13ac0c89538eb980331f8e0ed62fbeb49bf68
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-12-25 15:51:16 +0100

    Added support for drawArrays in GLOW.Elements (still need to add support in GLOW.Compiler)

--- src/core/Elements.js
+++ src/core/Elements.js
@@ -10,26 +10,35 @@ GLOW.Elements = (function() {
     // private data, functions and initializations here
 
     // constructor
-    function GLOWElements( data, type, usage ) {
+    function GLOWElements( data, type, usage, offset ) {
         this.id = GLOW.uniqueId();
-        this.elements = GL.createBuffer();
-        this.length = data.length;
         this.type = type !== undefined ? type : GL.TRIANGLES;
+        this.offset = offset !== undefined ? offset : 0;
 
-		if( !( data instanceof Uint16Array )) {
-			data = new Uint16Array( data );
-		}
+        if( typeof( data ) === "number" ) {
+            this.length = data;
+        } else {
+    		if( !( data instanceof Uint16Array )) {
+    			data = new Uint16Array( data );
+    		}
 
-        GL.bindBuffer( GL.ELEMENT_ARRAY_BUFFER, this.elements );
-        GL.bufferData( GL.ELEMENT_ARRAY_BUFFER, data, usage ? usage : GL.STATIC_DRAW );
+            this.length = data.length;
+            this.elements = GL.createBuffer();
+            GL.bindBuffer( GL.ELEMENT_ARRAY_BUFFER, this.elements );
+            GL.bufferData( GL.ELEMENT_ARRAY_BUFFER, data, usage ? usage : GL.STATIC_DRAW );
+        }
     }
 
     // methods
     GLOWElements.prototype.draw = function() {
-        if( !GLOW.currentContext.cache.elementsCached( this )) {
-             GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.elements );
+        if( this.elements !== undefined ) {
+            if( !GLOW.currentContext.cache.elementsCached( this )) {
+                 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.elements );
+            }
+            GL.drawElements( this.type, this.length, GL.UNSIGNED_SHORT, this.offset );
+        } else {
+            GL.drawArrays( this.type, this.offset, this.length );
         }
-        GL.drawElements( this.type, this.length, GL.UNSIGNED_SHORT, 0 );
     };
     
     GLOWElements.prototype.dispose = function() {
