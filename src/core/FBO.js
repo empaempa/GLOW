bc138cdf15cef7b8703d1918178a217cc20d3a0f
Mikael Emtinger <mikael.emtinger@gmail.com>
2012-04-16 20:16:16 +0200

    Split constructor into constructor and .createBuffers for easy resize. Added .deleteBuffers and .resize

--- src/core/FBO.js
+++ src/core/FBO.js
@@ -9,19 +9,19 @@ GLOW.FBO = (function() {
 
     	parameters = parameters !== undefined ? parameters : {};
 
-    	this.id            = GLOW.uniqueId();
-    	this.width         = parameters.width          !== undefined ? parameters.width          : parameters.size !== undefined ? parameters.size : window.innerWidth;
-    	this.height        = parameters.height         !== undefined ? parameters.height         : parameters.size !== undefined ? parameters.size : window.innerHeight;
-    	var wrapS          = parameters.wrapS          !== undefined ? parameters.wrapS          : parameters.wrap !== undefined ? parameters.wrap : GL.CLAMP_TO_EDGE;
-    	var wrapT          = parameters.wrapT          !== undefined ? parameters.wrapT          : parameters.wrap !== undefined ? parameters.wrap : GL.CLAMP_TO_EDGE;
-    	var magFilter      = parameters.magFilter      !== undefined ? parameters.magFilter      : GL.LINEAR;
-    	var minFilter      = parameters.minFilter      !== undefined ? parameters.minFilter      : GL.LINEAR;
-    	var internalFormat = parameters.internalFormat !== undefined ? parameters.internalFormat : GL.RGBA;
-    	var format         = parameters.format         !== undefined ? parameters.format         : GL.RGBA;
-    	var type           = parameters.type           !== undefined ? parameters.type           : GL.UNSIGNED_BYTE;
-    	var depth          = parameters.depth          !== undefined ? parameters.depth          : true;
-    	var stencil        = parameters.stencil        !== undefined ? parameters.stencil        : false;
-        var data           = parameters.data           !== undefined ? parameters.data           : null;
+    	this.id             = GLOW.uniqueId();
+    	this.width          = parameters.width  || parameters.size || window.innerWidth;
+    	this.height         = parameters.height || parameters.size || window.innerHeight;
+    	this.wrapS          = parameters.wrapS  || parameters.wrap || GL.CLAMP_TO_EDGE;
+    	this.wrapT          = parameters.wrapT  || parameters.wrap || GL.CLAMP_TO_EDGE;
+    	this.magFilter      = parameters.magFilter || parameters.filter || GL.LINEAR;
+    	this.minFilter      = parameters.minFilter || parameters.filter || GL.LINEAR;
+    	this.internalFormat = parameters.internalFormat || GL.RGBA;
+    	this.format         = parameters.format || GL.RGBA;
+    	this.type           = parameters.type || GL.UNSIGNED_BYTE;
+    	this.depth          = parameters.depth   !== undefined ? parameters.depth : true;
+    	this.stencil        = parameters.stencil !== undefined ? parameters.stencil : false;
+        this.data           = parameters.data || null;
 
         this.isBound       = false;
     	this.textureUnit   = -1;
@@ -56,81 +56,98 @@ GLOW.FBO = (function() {
     	} else {
         	this.clearSettings = { r: 0, g: 0, b: 0, a: 1, depth: 1, bits: 0 };
         	this.clearSettings.bits  = GL.COLOR_BUFFER_BIT;
-        	this.clearSettings.bits |= depth   ? GL.DEPTH_BUFFER_BIT   : 0;
-            this.clearSettings.bits |= stencil ? GL.STENCIL_BUFFER_BIT : 0;
+        	this.clearSettings.bits |= this.depth   ? GL.DEPTH_BUFFER_BIT   : 0;
+            this.clearSettings.bits |= this.stencil ? GL.STENCIL_BUFFER_BIT : 0;
     	}
 
-    	// setup texture
-    	this.texture = GL.createTexture();
-    	GL.bindTexture  ( this.textureType, this.texture );
-    	GL.texParameteri( this.textureType, GL.TEXTURE_WRAP_S, wrapS );
-    	GL.texParameteri( this.textureType, GL.TEXTURE_WRAP_T, wrapT );
-    	GL.texParameteri( this.textureType, GL.TEXTURE_MAG_FILTER, magFilter );
-    	GL.texParameteri( this.textureType, GL.TEXTURE_MIN_FILTER, minFilter );
+        this.createBuffers();
+	}
+
+    // methods
+    GLOWFBO.prototype.createBuffers = function() {
+        // setup texture
+        this.texture = GL.createTexture();
+        GL.bindTexture  ( this.textureType, this.texture );
+        GL.texParameteri( this.textureType, GL.TEXTURE_WRAP_S, this.wrapS );
+        GL.texParameteri( this.textureType, GL.TEXTURE_WRAP_T, this.wrapT );
+        GL.texParameteri( this.textureType, GL.TEXTURE_MAG_FILTER, this.magFilter );
+        GL.texParameteri( this.textureType, GL.TEXTURE_MIN_FILTER, this.minFilter );
 
         if( this.textureType === GL.TEXTURE_2D ) {
-            if( data === null || data instanceof Uint8Array ) {
-                GL.texImage2D( this.textureType, 0, internalFormat, this.width, this.height, 0, format, type, data );
+            if( this.data === null || this.data instanceof Uint8Array ) {
+                GL.texImage2D( this.textureType, 0, this.internalFormat, this.width, this.height, 0, this.format, this.type, this.data );
             } else {
-                GL.texImage2D( this.textureType, 0, internalFormat, format, type, data );
+                GL.texImage2D( this.textureType, 0, this.internalFormat, this.format, this.type, this.data );
             }
         } else {
             for( var c in cubeSideOffsets ) {
-            	GL.texImage2D( GL.TEXTURE_CUBE_MAP_POSITIVE_X + cubeSideOffsets[ c ], 0, internalFormat, this.width, this.height, 0, format, type, data[ c ] );
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_POSITIVE_X + cubeSideOffsets[ c ], 0, this.internalFormat, this.width, this.height, 0, this.format, this.type, this.data[ c ] );
             }
         }
 
         // setup render buffer
-        if( depth || stencil ) {
-        	this.renderBuffer = GL.createRenderbuffer();
-    		GL.bindRenderbuffer( GL.RENDERBUFFER, this.renderBuffer );
-    		if( depth && !stencil ) {
-    			GL.renderbufferStorage( GL.RENDERBUFFER, GL.DEPTH_COMPONENT16, this.width, this.height );
-    		} else if( !depth && stencil ) {
-    			GL.renderbufferStorage( GL.RENDERBUFFER, GL.STENCIL_INDEX8, this.width, this.height );
-    		} else if( depth && stencil ) {
-    			GL.renderbufferStorage( GL.RENDERBUFFER, GL.DEPTH_STENCIL, this.width, this.height );
-    		}
+        if( this.depth || this.stencil ) {
+            this.renderBuffer = GL.createRenderbuffer();
+            GL.bindRenderbuffer( GL.RENDERBUFFER, this.renderBuffer );
+            if( this.depth && !this.stencil ) {
+                GL.renderbufferStorage( GL.RENDERBUFFER, GL.DEPTH_COMPONENT16, this.width, this.height );
+            } else if( !this.depth && this.stencil ) {
+                GL.renderbufferStorage( GL.RENDERBUFFER, GL.STENCIL_INDEX8, this.width, this.height );
+            } else if( this.depth && this.stencil ) {
+                GL.renderbufferStorage( GL.RENDERBUFFER, GL.DEPTH_STENCIL, this.width, this.height );
+            }
         }
 
 
         // setup frame buffer
         if( this.textureType === GL.TEXTURE_2D ) {
-        	this.frameBuffer = GL.createFramebuffer();
-    		GL.bindFramebuffer( GL.FRAMEBUFFER, this.frameBuffer );
-    		GL.framebufferTexture2D( GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, this.texture, 0 );
+            this.frameBuffer = GL.createFramebuffer();
+            GL.bindFramebuffer( GL.FRAMEBUFFER, this.frameBuffer );
+            GL.framebufferTexture2D( GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, this.texture, 0 );
 
-    		if( depth && !stencil ) {
-    			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-    		} else if( !depth && stencil ) {
-    			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-    		} else if( depth && stencil ) {
-    			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-    		}
+            if( this.depth && !this.stencil ) {
+                GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+            } else if( !this.depth && this.stencil ) {
+                GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+            } else if( this.depth && this.stencil ) {
+                GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+            }
         } else {
-        	this.frameBuffers = {};
-        	for( var f in cubeSideOffsets ) {
-        	    this.frameBuffers[ f ] = GL.createFramebuffer();
-        		GL.bindFramebuffer( GL.FRAMEBUFFER, this.frameBuffers[ f ] );
-        		GL.framebufferTexture2D( GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_CUBE_MAP_POSITIVE_X + cubeSideOffsets[ f ], this.texture, 0 );
+            this.frameBuffers = {};
+            for( var f in cubeSideOffsets ) {
+                this.frameBuffers[ f ] = GL.createFramebuffer();
+                GL.bindFramebuffer( GL.FRAMEBUFFER, this.frameBuffers[ f ] );
+                GL.framebufferTexture2D( GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_CUBE_MAP_POSITIVE_X + cubeSideOffsets[ f ], this.texture, 0 );
 
-        		if( depth && !stencil ) {
-        			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-        		} else if( !depth && stencil ) {
-        			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-        		} else if( depth && stencil ) {
-        			GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
-        		}
-        	}
+                if( this.depth && !this.stencil ) {
+                    GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+                } else if( !this.depth && this.stencil ) {
+                    GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+                } else if( this.depth && this.stencil ) {
+                    GL.framebufferRenderbuffer( GL.FRAMEBUFFER, GL.DEPTH_STENCIL_ATTACHMENT, GL.RENDERBUFFER, this.renderBuffer );
+                }
+            }
         }
-    	
-		// release
-		GL.bindTexture( this.textureType, null );
-		GL.bindRenderbuffer( GL.RENDERBUFFER, null );
-		GL.bindFramebuffer( GL.FRAMEBUFFER, null);
-	}
+        
+        // release
+        GL.bindTexture( this.textureType, null );
+        GL.bindRenderbuffer( GL.RENDERBUFFER, null );
+        GL.bindFramebuffer( GL.FRAMEBUFFER, null);
+    }
 
-    // methods
+    GLOWFBO.prototype.deleteBuffers = function() {
+        if( this.texture      ) GL.deleteTexture( this.texture );
+        if( this.renderBuffer ) GL.deleteRenderbuffer( this.renderBuffer );
+
+        if( this.textureType === GL.TEXTURE_2D ) {
+            if( this.frameBuffer ) GL.deleteFramebuffer( this.frameBuffer );
+        } else {
+            for( var f in cubeSideOffsets ) {
+                GL.deleteFramebuffer( this.frameBuffers[ f ] );
+            }
+        }
+    }
+
     GLOWFBO.prototype.init = function() {
         // called from compiler but there's really nothing to do here
     };
@@ -202,8 +219,18 @@ GLOW.FBO = (function() {
     	return this;
     };
     
-    GLOWFBO.prototype.resize = function() {
-    	// TODO
+    GLOWFBO.prototype.resize = function( newWidth, newHeight ) {
+        var widthFactor  = newWidth  / this.width;
+        var heightFactor = newHeight / this.height; 
+
+        this.viewport.width  = this.viewport.width  * widthFactor;
+        this.viewport.height = this.viewport.height * heightFactor; 
+
+        this.width  = newWidth;
+        this.height = newHeight;
+
+        this.deleteBuffers();
+        this.createBuffers();
     	return this;
     };
 
