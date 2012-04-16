aade58194df47e760c93ac5c725922ff179d7f39
Mikael Emtinger <mikael.emtinger@gmail.com>
2012-04-16 20:11:20 +0200

    Added .log .warn .error and flags for filtering these out

--- src/GLOW.js
+++ src/GLOW.js
@@ -5,10 +5,16 @@
 var GLOW = (function() {
     "use strict"; "use restrict";
 
+    console.log( "Hello! My name is GLOW. Great to be around." );
+
     var glow = {};
     var contexts = {};
     var uniqueIdCounter = -1;
 
+    glow.LOGS     = 1;
+    glow.WARNINGS = 2;
+    glow.ERRORS   = 4;
+    glow.logging  = glow.ERRORS;// glow.LOGS | glow.WARNINGS | glow.ERRORS;
     glow.currentContext = {};
 
     glow.registerContext = function( context ) {
@@ -20,7 +26,7 @@ var GLOW = (function() {
         if( contexts[ id ] ) {
             return contexts[ id ];
         }
-        console.error( "Couldn't find context id " + id + ", returning current with id " + glow.currentContext.id );
+        GLOW.error( "Couldn't find context id " + id + ", returning current with id " + glow.currentContext.id );
         return glow.currentContext;
     };
 
@@ -36,6 +42,21 @@ var GLOW = (function() {
     glow.uniqueId = function() {
         return ++uniqueIdCounter;
     };
+
+    glow.log = function( message ) {
+        if( glow.logging & glow.LOGS ) 
+            console.log( message );
+    }
+
+    glow.warn = function( message ) {
+        if( glow.logging & glow.WARNINGS ) 
+            console.warn( message );
+    }
+
+    glow.error = function( message ) {
+        if( glow.logging  & glow.ERRORS ) 
+            console.error( message );
+    }
 
     return glow;
 }());
